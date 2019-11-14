7f897a571be30305b969012e80a93469dac37a63
vickyliu1220 <yunshl2@uw.edu>
2019-11-13 21:37:04 -0800

    dataset

--- dataset.R
+++ dataset.R
@@ -1,1 +1,10 @@
-potential_data <- read.csv("~/dataset/united-states-renewable-energy-technical-potential-1.csv", header = TRUE, stringsAsFactors = FALSE)
+
+
+potential_data <- read.csv("dataset/united-states-renewable-energy-technical-potential-1.csv",
+                           
+                           stringsAsFactors = FALSE, header = TRUE)
+
+
+annual_generation <- read.csv("dataset/annual_generation_state.csv",
+                              header = TRUE,
+                              stringsAsFactors = FALSE)
