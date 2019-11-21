ce2f273f8e8f2d5c25fd52e104ca92d349126edd
vickyliu1220 <yunshl2@uw.edu>
2019-11-21 07:17:35 -0800

    shinyapp

--- ui.R
+++ ui.R
@@ -19,7 +19,29 @@ library(plotly)
 
 # Define UI for application that draws a histogram
 page_one <- tabPanel(
-  "Introduction"# label for the tab in the navbar
+  "Introduction",
+  
+  p("Link to the technical report: https://github.com/truong1j/Info201_project/wiki/Technical-Report"),
+  h2("Problem Situation"),
+  p("'Wind turbines, solar panels and electric vehicles are spreading 
+    far more quickly around the world than many experts had predicted.
+    But this rapid growth in clean energy isn’t yet fast enough to slash
+    humanity’s greenhouse gas emissions and get global warming under control'
+    (New York Times). Washington is said to be one of the nation’s largest 
+    producers of renewable energy and we hope to analyze the production potential 
+    and actual generation of green energy across the United States. Within green energy production, 
+    we find that there are many stakeholders including but not limited to energy companies, energy 
+    consumers (residential, commercial, industrial, and transportation sectors), and environmentalists,
+    to mention a few. A piece of policy that touches on the topic of green energy is Washington state 
+    passing law requiring 100% clean energy by 2045. Beyond this law, the country has seen a rise in 
+    interest in other legislation such as the Green New Deal, which has become a point of campaign for 
+    Democratic candidates leading up to the 2020 election."),
+  h2("What is the problem?"),
+  p("There are ample areas for improvement in terms of energy efficiency, 
+    one of which is utilizing renewable energy production across the United States
+    that are not being used to their full potential. We will find which states are 
+    using fully reaching their potential and which states are not by comparing their 
+    current production to the potentiality of their production.")
 )
 
 # Define content for the second page
@@ -27,12 +49,13 @@ page_two <- tabPanel(
   "Current energy data", 
   p("Interactive map outlining the current energy production in the US tbd. Couldnt get it to load.
     BTW, merging is hard :) ")
+  # label for the tab in the navbar
 )
 
 # Define content for the third page
 page_three <- tabPanel(
   "Potential energy data", # label for the tab in the navbar
-  titlePanel("Energy Production by State"),
+  titlePanel("Energy Production by State(Potential)"),
   sidebarLayout(     
     
     
@@ -70,8 +93,8 @@ page_four <- tabPanel(
   )
 )
 page_five <- tabPanel(
-  "Conclusion" # label for the tab in the navbar
-  # ...more content would go here...
+  "Conclusion",# label for the tab in the navbar
+  p("Conclusion will be updated for the final project deliervable...")
 )
 
 
